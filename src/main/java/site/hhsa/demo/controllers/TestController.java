aa393d280a6c734c113caba04cabe2ae9ffdd6fd
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-09-17 10:37:13 -0500

    First WORKING commit

---
+++ src/main/java/site/hhsa/demo/controllers/TestController.java
@@ -0,0 +1,15 @@
+package site.hhsa.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/home")
+    private String test() {
+        return "index";
+    }
+
+}
