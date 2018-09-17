fc0b68bbedb49bfd0b0871c3f7b17f3e0f6eabf4
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-09-17 10:06:09 -0500

    First commit

---
+++ src/main/java/site/hhsa/demo/HelpingHandsApplication.java
@@ -0,0 +1,12 @@
+package site.hhsa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpingHandsApplication.class, args);
+    }
+}
