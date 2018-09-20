ae01314988175eb76137e3c2fc059285d9f79da2
mkovar <mkovar@mkovar.local>
2018-09-20 15:17:59 -0500

    updated homepage, org dash and user controller

---
+++ src/main/java/site/hhsa/demo/users/controllers/UserController.java
@@ -0,0 +1,32 @@
+package site.hhsa.demo.users.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import site.hhsa.demo.organizations.models.Organization;
+import site.hhsa.demo.organizations.repositories.EventRepo;
+import site.hhsa.demo.organizations.repositories.OrgRepo;
+import site.hhsa.demo.users.models.User;
+import site.hhsa.demo.users.repositories.UserRepo;
+
+@Controller
+public class UserController {
+
+    UserRepo userDao;
+
+    public UserController(UserRepo userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/{org_name}/create/event")
+    private String userCreateEvent(@PathVariable String org_name, Model viewModel, @ModelAttribute UserRepo userDao){
+        viewModel.addAttribute("org_name", userDao);
+
+
+        return "organizations/dashboard";
+    }
+
+
+}
