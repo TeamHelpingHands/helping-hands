8c31643493af90f53a457fb8c78bd9eba7942419
vlewisGH <vincent.sean.lewis@gmail.com>
2018-09-23 00:54:04 -0500

    added 'follow' route and mass text are sent out when an event is created

--- src/main/java/site/hhsa/demo/services/MassMessenger.java
+++ src/main/java/site/hhsa/demo/services/MassMessenger.java
@@ -1,14 +1,15 @@
 package site.hhsa.demo.services;
 
 import site.hhsa.demo.users.models.User;
+import site.hhsa.demo.volunteers.models.Volunteer;
 
 import java.util.List;
 
 public class MassMessenger {
 
-    public MassMessenger(List<User> followers, String message) {
-        for(User follower : followers){
-            new SmsSender().SmsSender(follower.getPhnNum(),message);
+    public MassMessenger(List<Volunteer> followers, String message) {
+        for(Volunteer follower : followers){
+            new SmsSender().SmsSender(follower.getUser().getPhnNum(),message);
         }
     }
 }
